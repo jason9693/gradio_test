e942a8e349cfbf57f8279013a6a10a28faa8a787
root <root@brain-cluster-gpu16.dakao.io>
2021-10-25 23:34:38 +0900

    add gpt tok

--- attention.py
+++ attention.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig
 import gradio as gr
 from torch.nn import functional as F
 import seaborn
@@ -8,15 +8,12 @@ from PIL import Image
 
 import matplotlib.font_manager as fm
 
+import util
+
 font_path = r'NanumGothicCoding.ttf'
 fontprop = fm.FontProperties(fname=font_path, size=18)
 
 plt.rcParams["font.family"] = 'NanumGothic'
-hatespeech_category_map = {
-    "0": "일반글",
-    "1": "공격발언",
-    "2": "차별발언"
-}
 
 
 def visualize_attention(sent, attention_matrix, n_words=10):
@@ -35,28 +32,6 @@ def visualize_attention(sent, attention_matrix, n_words=10):
  
     fig.tight_layout()
     plt.close()
-    # fig, axs = plt.subplots(1,6, figsize=(20, 10))
-
-    # for layer in range(1, 12, 2):
-    #     print("Encoder Layer", layer+1)
-    #     draw(attention_matrix[layer], sent, sent if layer < 2 else [], ax=axs[int(layer/2)])
-    
-    # plt.show()
-    # return fig
-        
-    # for layer in range(1, 6, 2):
-    #     fig, axs = plt.subplots(1,4, figsize=(20, 10))
-    #     print("Decoder Self Layer", layer+1)
-    #     for h in range(4):
-    #         draw(model.decoder.layers[layer].self_attn.attn[0, h].data[:len(tgt_sent), :len(tgt_sent)], 
-    #             tgt_sent, tgt_sent if h ==0 else [], ax=axs[h])
-    #     plt.show()
-    #     print("Decoder Src Layer", layer+1)
-    #     fig, axs = plt.subplots(1,4, figsize=(20, 10))
-    #     for h in range(4):
-    #         draw(model.decoder.layers[layer].self_attn.attn[0, h].data[:len(tgt_sent), :len(sent)], 
-    #             sent, tgt_sent if h ==0 else [], ax=axs[h])
-    #     plt.show()
 
     return fig
 
@@ -66,16 +41,24 @@ def predict(model_name, text):
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    config = AutoConfig.from_pretrained(model_name)
+    print(config.id2label)
 
     tokenized_text = tokenizer([text], return_tensors='pt')
+
+    input_tokens = tokenizer.convert_ids_to_tokens(tokenized_text.input_ids[0])
+    print(input_tokens)
+    input_tokens = util.bytetokens_to_unicdode(input_tokens) if config.model_type in ['roberta', 'gpt', 'gpt2'] else input_tokens
+
     model.eval()
     output, attention = model(**tokenized_text, output_attentions=True, return_dict=False)
     output = F.softmax(output, dim=-1)
     result = {}
     
     for idx, label in enumerate(output[0].detach().numpy()):
-        result[hatespeech_category_map[str(idx)]] = float(label)
-    fig = visualize_attention(tokenizer.convert_ids_to_tokens(tokenized_text.input_ids[0]), attention[0][0].detach().numpy())
+        result[config.id2label[idx]] = float(label)
+
+    fig = visualize_attention(input_tokens, attention[0][0].detach().numpy())
     return result, fig#.logits.detach()#.numpy()#, output.attentions.detach().numpy()
 
 
@@ -89,5 +72,5 @@ if __name__ == '__main__':
 
 
     #Create a gradio app with a button that calls predict()
-    app = gr.Interface(fn=predict, server_port=22333, server_name='0.0.0.0', inputs=['text', 'text'], outputs=['label', 'plot'])
+    app = gr.Interface(fn=predict, server_port=26899, server_name='0.0.0.0', inputs=['text', 'text'], outputs=['label', 'plot'], examples = [[model_name, text]])
     app.launch(inline=False)
